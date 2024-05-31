6e904c466a58a54ad0ab8380f0c9acfce75f0ac7
Qharny <cannycc1020@gmail.com>
2024-05-31 22:43:27 +0000

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   .vscode/settings.json
    	modified:   android/app/build.gradle
    	new file:   android/app/google-services.json
    	modified:   android/build.gradle
    	modified:   android/settings.gradle
    	new file:   firebase.json
    	new file:   lib/Screen/id.txt
    	new file:   lib/firebase_options.dart
    	modified:   lib/main.dart
    	modified:   macos/Flutter/GeneratedPluginRegistrant.swift
    	modified:   pubspec.lock
    	modified:   pubspec.yaml
    	modified:   windows/flutter/generated_plugin_registrant.cc
    	modified:   windows/flutter/generated_plugins.cmake

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:otp/Screen/login.dart';
+import 'package:otp/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
